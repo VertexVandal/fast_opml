f7d08806b789e776786171a4ea4a7320a478b5f2
VertexVandal <VertexVandal>
2024-04-17 23:41:48 +0200

    fix

--- lib/fast_opml.ex
+++ lib/fast_opml.ex
@@ -1,12 +1,15 @@
 defmodule FastOpml do
+  use Rustler, otp_app: :fast_opml, crate: "fast_opml"
 
+  # When your NIF is loaded, it will override this function.
+  def parse_opml(_a), do: :erlang.nif_error(:nif_not_loaded)
 
-  defmodule Native do
-    use Rustler, otp_app: :fast_opml, crate: "fast_opml"
+  # defmodule Native do
+  #   use Rustler, otp_app: :fast_opml, crate: "fast_opml"
 
-    # When your NIF is loaded, it will override this function.
-    def parse_opml(_a), do: :erlang.nif_error(:nif_not_loaded)
-  end
+  #   # When your NIF is loaded, it will override this function.
+  #   def parse_opml(_a), do: :erlang.nif_error(:nif_not_loaded)
+  # end
 
-  def parse_opml(opml_string), do: FastOpml.Native.parse_opml(opml_string)
+  # def parse_opml(opml_string), do: FastOpml.Native.parse_opml(opml_string)
 end
