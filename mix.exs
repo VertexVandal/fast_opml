5501b1c40431612fe69da63bd49ea55f43a29bec
VertexVandal <VertexVandal>
2024-04-17 23:20:21 +0200

    version downgrade

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule FastOpml.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, "~> 0.32.1", runtime: false}
+      {:rustler, "~> 0.29.0", runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
