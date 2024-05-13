e80b2e207a13b449c4ced76c79a4dacab7cfbb75
VertexVandal <128510669+VertexVandal@users.noreply.github.com>
2024-05-14 01:36:45 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FastOpml.MixProject do
     [
       app: :fast_opml,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.14.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
